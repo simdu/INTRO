f05b4b54474bc5050b6a9dc833b8ec2a77171bf7
Manuel Felber <manuel.felber@stud.hslu.ch>
2016-09-30 14:18:35 +0200

    frmd: correct debugger, fix project renaming bug

--- frdm/Sources/main.c
+++ frdm/Sources/main.c
@@ -30,6 +30,12 @@
 /* Including needed modules to compile this module/procedure */
 #include "Cpu.h"
 #include "Events.h"
+#include "LEDPin1.h"
+#include "BitIoLdd10.h"
+#include "LEDPin2.h"
+#include "BitIoLdd11.h"
+#include "LEDPin3.h"
+#include "BitIoLdd12.h"
 #include "WAIT1.h"
 #include "CS1.h"
 #include "HF1.h"
@@ -55,7 +61,9 @@ int main(void)
   /*** End of Processor Expert internal initialization.                    ***/
 
   /* Write your code here */
-  /* For example: for(;;) { } */
+  for(;;) {
+
+  }
   APP_Start();
 
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
