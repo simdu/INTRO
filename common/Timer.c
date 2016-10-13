46ef6500b8d09bffc0fb1130f6235be4f6f191f2
Manuel Felber <manuel.felber@bluewin.ch>
2016-10-13 16:30:30 +0200

    frdm: with defines in timer

--- common/Timer.c
+++ common/Timer.c
@@ -24,10 +24,12 @@ void TMR_OnInterrupt(void) {
 	static int count_OnInterrupt = 0;
 
 	count_OnInterrupt++;
+#if PL_CONFIG_HAS_EVENTS
 	if(count_OnInterrupt >= (1000/TMR_TICK_MS)){
 		EVNT_SetEvent(EVNT_LED_HEARTBEAT);
 		count_OnInterrupt = 0;
 	}
+#endif
 }
 
 void TMR_Init(void) {
