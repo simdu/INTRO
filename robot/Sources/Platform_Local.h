4eea8f4e28b697f49a5eddb38f0941b9d4fa793d
Manuel Felber <manuel.felber@bluewin.ch>
2016-11-17 15:37:43 +0100

    robot: enable motor components

--- robot/Sources/Platform_Local.h
+++ robot/Sources/Platform_Local.h
@@ -54,8 +54,8 @@
 #define PL_LOCAL_CONFIG_HAS_LCD_MENU_DISABLED             /* disable LCD menu */
 
 /* robot hardware functionality */
-#define PL_LOCAL_CONFIG_HAS_MOTOR_DISABLED                /* disable motor */
-#define PL_LOCAL_CONFIG_HAS_REFLECTANCE_DISABLED          /* disable IR reflectance sensor */
+//#define PL_LOCAL_CONFIG_HAS_MOTOR_DISABLED                /* disable motor */
+//#define PL_LOCAL_CONFIG_HAS_REFLECTANCE_DISABLED          /* disable IR reflectance sensor */
 #define PL_LOCAL_CONFIG_HAS_QUADRATURE_DISABLED           /* disable quadrature encoder */
 #define PL_LOCAL_CONFIG_HAS_MOTOR_TACHO_DISABLED          /* disable tacho */
 #define PL_LOCAL_CONFIG_HAS_MPC4728_DISABLED              /* disable MPC4728 (only for V1 robot) */
