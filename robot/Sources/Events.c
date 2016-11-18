a34bdf5afc79af03e64d13e40aa00eff1d787ea5
Manuel Felber <manuel.felber@bluewin.ch>
2016-11-18 16:04:44 +0100

    robot: implement Tacho

--- robot/Sources/Events.c
+++ robot/Sources/Events.c
@@ -32,6 +32,7 @@
 #include "Timer.h"
 #include "Q4CLeft.h"
 #include "Q4CRight.h"
+#include "Tacho.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -138,8 +139,7 @@ void FRTOS1_vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName)
 */
 void FRTOS1_vApplicationTickHook(void)
 {
-  /* Called for every RTOS tick. */
-  /* Write your code here ... */
+	TACHO_Sample();
 }
 
 /*
